f91b81f31de50bb5382a9b6e07697c3338c3e767
ishawagle1 <144363343+ishawagle1@users.noreply.github.com>
2024-12-13 02:39:55 -0500

    Update FinalProjectRPiCode.py

--- FinalProjectRPiCode.py
+++ FinalProjectRPiCode.py
@@ -11,69 +11,69 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from gpiozero import InputDevice
 from gpiozero import PWMOutputDevice
+
+#setting up the GPIO used for the fan
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 
+#try catch so that exit behavior can be specified. 
 try:
+    #defines the motor hat and pin for the fan and flame sensor
     mh = Raspi_MotorHAT(0x70)
     fan_pin = 27
-    
+    flame_sensor = InputDevice(17)
+
+    #specifies exit behavior of stepper motors 
     def turnOffStepper():
         #GPIO.output(fan_pin, GPIO.LOW)
         mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
         mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
         mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
         mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
-    
-    def turnOffFan():
-        GPIO.output(fan_pin, GPIO.LOW)
         
     atexit.register(turnOffStepper)
-    atexit.register(turnOffFan)
-    
+
+    #defines the directions relative to the platform based on stepper rotation
     right = Raspi_MotorHAT.FORWARD
     left = Raspi_MotorHAT.BACKWARD
+
+    #initializes the two stepper motors along with their speeds
     myStepper = mh.getStepper(200,1)
     myStepper.setSpeed(90)
     turntable = mh.getStepper(200, 2)
     turntable.setSpeed(90)
-    one_note = 105
+    steps = 105
     position = 0
     fireDetected = False
     facingRightDirection = False
     senseFlame = False
     sweeping = False
-    flame_sensor = InputDevice(17)
 
-
-    def moveRight(note):
+    def moveRight(amount):
         global position
         print("MOVING RIGHT")
-        myStepper.step(one_note*(note), right, Raspi_MotorHAT.SINGLE)
+        myStepper.step(steps*(amount), right, Raspi_MotorHAT.SINGLE)
         time.sleep(0.05)
         turnOffStepper()
-        position += one_note*(note)
-        
-        
-    def moveLeft(note):
-        myStepper.step(one_note*(note), left, Raspi_MotorHAT.SINGLE)
-        time.sleep(1)
-        turnOffStepper()
+        #keeps track of how much the stepper has moved right so that it can eventually go back home
+        position += steps*(amount)
 
-        
     def goHome():
         global position
         print("GOING HOME")
+        #moves left by the position you have moved right
         myStepper.step(position, left, Raspi_MotorHAT.SINGLE)
         time.sleep(1)
         turnOffStepper()
+        #reset the direction boolean
         facingRightDirection = False
-        
+
+    #turns the platform stepper 180 degrees 
     def turn180():
-        turntable.step(105, left, Raspi_MotorHAT.SINGLE)
+        turntable.step(steps, left, Raspi_MotorHAT.SINGLE)
         turnOffStepper()
 
-
+    #camera initialization
     i2c = busio.I2C(board.SCL, board.SDA, frequency = 800000)
 
     mlx = adafruit_mlx90640.MLX90640(i2c)
@@ -88,6 +88,7 @@ try:
     heatmap = ax.imshow(np.zeros((24,32)), cmap = 'hsv', vmin = 20, vmax = 40)
     plt.colorbar(heatmap)
 
+    #function that turns on the fan on the platform
     def blow():
         GPIO.output(fan_pin, GPIO.HIGH)
         time.sleep(2)
@@ -95,20 +96,25 @@ try:
         GPIO.output(fan_pin, GPIO.LOW)
         time.sleep(1)
         
-        
+    #the sweeping feedback loop
     def sweep(facingRightDirection):
         global sweeping
         global position
         global fireDetected
         counter = 0
         print("in sweep")
+        #keep moving right until you sense the flame
         while(not senseFlame):
+            #ensures that the platform doesn't move more than the conveyor belt allows
             counter = counter +1
             if(counter >10):
                 break
             moveRight(1)
+            #after you move right, check if the flame sensor has detected the fire. 
             if not flame_sensor.is_active:
                 print("detected")
+                #if it is facing the right direction, when it detects the fire it should turn right towards the fire and 
+                #extinguish it
                 if(facingRightDirection):
                     turntable.step(23, right, Raspi_MotorHAT.SINGLE)
                     turnOffStepper()
@@ -116,8 +122,9 @@ try:
                     print("BLOWINGGGG")
                     time.sleep(1)
                     turntable.step(23, left, Raspi_MotorHAT.SINGLE)
+                #otherwise turn left and reset
                 else:
-                    turntable.step(21, left, Raspi_MotorHAT.SINGLE)
+                    turntable.step(23, left, Raspi_MotorHAT.SINGLE)
                     turnOffStepper()
                     blow()
                     print("BLOWINGGGG")
@@ -126,12 +133,14 @@ try:
 
                 break
             time.sleep(0.25)
+        #after the fire has been detected, go home and reset all booleans
         goHome()
         position = 0
         sweeping = False
         fireDetected = False
         return
-        
+
+    #function that is called that refreshes the camera
     def read_mlx90640():
         global fireDetected
         global facingRightDirection
@@ -144,6 +153,7 @@ try:
         except ValueError:
             return None
         count = 0
+        #calculating the average H location of the flame
         averageH = -1;
         for h in range(24):
             for w in range(32):
@@ -158,27 +168,26 @@ try:
             averageH = averageH / count
         
         print(averageH)
-        #Code to simulate facing forward or backward stepper movement
+        #Determines whether the flame sensor is facing the right direction or not
         if(fireDetected and averageH < 8):
             facingRightDirection = True
         else:
             facingRightDirection = False
-        
+
+        #turns if the flame sensor is not facing the right direction
         if(fireDetected and not facingRightDirection):
             facingRightDirection = False
             turn180()
             print("Sleeping")
             time.sleep(2)
             print("awake")
-            #facingRightDirection = True
-            
+
+        #begin the sweeping feedback loop
         if(fireDetected and not sweeping):
             print("Begin sweeping!")
-            #sweeping = True
             sweep(facingRightDirection)
             if(not facingRightDirection):
-                turntable.step(105, right, Raspi_MotorHAT.SINGLE)
-                turnOffStepper()
+                turn180()
                 facingRightDirection = False
         
         print(fireDetected)
@@ -186,35 +195,25 @@ try:
 
         return np.array(frame)
 
-    while True:
-        read_mlx90640()
+   
+    #update the heatmap display and refresh the frames    
+    def update_heatmap(*args):
+        temp_data = read_mlx90640()
+        if temp_data is not None:
+            reshaped_data = temp_data.reshape((24,32))
+            heatmap.set_array(reshaped_data)
         GPIO.output(fan_pin, GPIO.LOW)
+        return heatmap,
         
-    #def update_heatmap(*args):
-    #    temp_data = read_mlx90640()
-    #    if temp_data is not None:
-    #        reshaped_data = temp_data.reshape((24,32))
-    #        heatmap.set_array(reshaped_data)
-    #    GPIO.output(fan_pin, GPIO.LOW)
+    ani = animation.FuncAnimation(fig, update_heatmap, interval = 500)
 
-    #    return heatmap,
-        
-    #ani = animation.FuncAnimation(fig, update_heatmap, interval = 500)
+    plt.show()
 
-    #plt.show()
-
+#cleaning up when exiting
 except KeyboardInterrupt:
-    
     print("exiting...")
     GPIO.output(fan_pin, GPIO.LOW)
     time.sleep(1)
     print("exited")
-    #GPIO.cleanup()
+    GPIO.cleanup()
     
-
-#finally:
-    #GPIO.output(fan_pin, GPIO.LOW)
-    #GPIO.cleanup()
-    
-#GPIO.output(fan_pin, GPIO.LOW)
-#GPIO.cleanup()
