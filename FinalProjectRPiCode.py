c9c5515ec474f993a49406b609edb2e562c4a453
ishawagle1 <144363343+ishawagle1@users.noreply.github.com>
2024-12-13 02:11:18 -0500

    Create FinalProjectRPiCode.py

---
+++ FinalProjectRPiCode.py
@@ -0,0 +1,220 @@
+import smbus2
+import time
+import board
+import busio
+from Raspi_MotorHAT import Raspi_MotorHAT
+import RPi.GPIO as GPIO
+import atexit
+import adafruit_mlx90640
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from gpiozero import InputDevice
+from gpiozero import PWMOutputDevice
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.OUT)
+
+try:
+    mh = Raspi_MotorHAT(0x70)
+    fan_pin = 27
+    
+    def turnOffStepper():
+        #GPIO.output(fan_pin, GPIO.LOW)
+        mh.getMotor(1).run(Raspi_MotorHAT.RELEASE)
+        mh.getMotor(2).run(Raspi_MotorHAT.RELEASE)
+        mh.getMotor(3).run(Raspi_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Raspi_MotorHAT.RELEASE)
+    
+    def turnOffFan():
+        GPIO.output(fan_pin, GPIO.LOW)
+        
+    atexit.register(turnOffStepper)
+    atexit.register(turnOffFan)
+    
+    right = Raspi_MotorHAT.FORWARD
+    left = Raspi_MotorHAT.BACKWARD
+    myStepper = mh.getStepper(200,1)
+    myStepper.setSpeed(90)
+    turntable = mh.getStepper(200, 2)
+    turntable.setSpeed(90)
+    one_note = 105
+    position = 0
+    fireDetected = False
+    facingRightDirection = False
+    senseFlame = False
+    sweeping = False
+    flame_sensor = InputDevice(17)
+
+
+    def moveRight(note):
+        global position
+        print("MOVING RIGHT")
+        myStepper.step(one_note*(note), right, Raspi_MotorHAT.SINGLE)
+        time.sleep(0.05)
+        turnOffStepper()
+        position += one_note*(note)
+        
+        
+    def moveLeft(note):
+        myStepper.step(one_note*(note), left, Raspi_MotorHAT.SINGLE)
+        time.sleep(1)
+        turnOffStepper()
+
+        
+    def goHome():
+        global position
+        print("GOING HOME")
+        myStepper.step(position, left, Raspi_MotorHAT.SINGLE)
+        time.sleep(1)
+        turnOffStepper()
+        facingRightDirection = False
+        
+    def turn180():
+        turntable.step(105, left, Raspi_MotorHAT.SINGLE)
+        turnOffStepper()
+
+
+    i2c = busio.I2C(board.SCL, board.SDA, frequency = 800000)
+
+    mlx = adafruit_mlx90640.MLX90640(i2c)
+    print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+
+    mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+
+    frame = [0] * 768
+
+    #create figure
+    fig, ax = plt.subplots()
+    heatmap = ax.imshow(np.zeros((24,32)), cmap = 'hsv', vmin = 20, vmax = 40)
+    plt.colorbar(heatmap)
+
+    def blow():
+        GPIO.output(fan_pin, GPIO.HIGH)
+        time.sleep(2)
+        print("fan off")
+        GPIO.output(fan_pin, GPIO.LOW)
+        time.sleep(1)
+        
+        
+    def sweep(facingRightDirection):
+        global sweeping
+        global position
+        global fireDetected
+        counter = 0
+        print("in sweep")
+        while(not senseFlame):
+            counter = counter +1
+            if(counter >10):
+                break
+            moveRight(1)
+            if not flame_sensor.is_active:
+                print("detected")
+                if(facingRightDirection):
+                    turntable.step(23, right, Raspi_MotorHAT.SINGLE)
+                    turnOffStepper()
+                    blow()
+                    print("BLOWINGGGG")
+                    time.sleep(1)
+                    turntable.step(23, left, Raspi_MotorHAT.SINGLE)
+                else:
+                    turntable.step(21, left, Raspi_MotorHAT.SINGLE)
+                    turnOffStepper()
+                    blow()
+                    print("BLOWINGGGG")
+                    time.sleep(1)
+                    turntable.step(21, right, Raspi_MotorHAT.SINGLE)
+
+                break
+            time.sleep(0.25)
+        goHome()
+        position = 0
+        sweeping = False
+        fireDetected = False
+        return
+        
+    def read_mlx90640():
+        global fireDetected
+        global facingRightDirection
+        global sweeping
+        global frame
+        frame = [33] * 768
+        try:
+            mlx.getFrame(frame)
+            print("updating frames")
+        except ValueError:
+            return None
+        count = 0
+        averageH = -1;
+        for h in range(24):
+            for w in range(32):
+                t = frame[h*32 + w]
+                if(t>36):
+                    print(t," ", h, " ",w)
+                    averageH += h
+                    fireDetected = True
+                    count+=1;
+        
+        if(count != 0):
+            averageH = averageH / count
+        
+        print(averageH)
+        #Code to simulate facing forward or backward stepper movement
+        if(fireDetected and averageH < 8):
+            facingRightDirection = True
+        else:
+            facingRightDirection = False
+        
+        if(fireDetected and not facingRightDirection):
+            facingRightDirection = False
+            turn180()
+            print("Sleeping")
+            time.sleep(2)
+            print("awake")
+            #facingRightDirection = True
+            
+        if(fireDetected and not sweeping):
+            print("Begin sweeping!")
+            #sweeping = True
+            sweep(facingRightDirection)
+            if(not facingRightDirection):
+                turntable.step(105, right, Raspi_MotorHAT.SINGLE)
+                turnOffStepper()
+                facingRightDirection = False
+        
+        print(fireDetected)
+        GPIO.output(fan_pin, GPIO.LOW)
+
+        return np.array(frame)
+
+    while True:
+        read_mlx90640()
+        GPIO.output(fan_pin, GPIO.LOW)
+        
+    #def update_heatmap(*args):
+    #    temp_data = read_mlx90640()
+    #    if temp_data is not None:
+    #        reshaped_data = temp_data.reshape((24,32))
+    #        heatmap.set_array(reshaped_data)
+    #    GPIO.output(fan_pin, GPIO.LOW)
+
+    #    return heatmap,
+        
+    #ani = animation.FuncAnimation(fig, update_heatmap, interval = 500)
+
+    #plt.show()
+
+except KeyboardInterrupt:
+    
+    print("exiting...")
+    GPIO.output(fan_pin, GPIO.LOW)
+    time.sleep(1)
+    print("exited")
+    #GPIO.cleanup()
+    
+
+#finally:
+    #GPIO.output(fan_pin, GPIO.LOW)
+    #GPIO.cleanup()
+    
+#GPIO.output(fan_pin, GPIO.LOW)
+#GPIO.cleanup()
